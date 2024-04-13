756d20d8b16539163637f4d7186c3002db5fb552
Wasif Jameel <wasif.jameel1@gmail.com>
2024-04-13 16:08:33 +0500

    get and post routes

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,32 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get('/', async (c) => {
+  try {
+    // let body; = (await c.req.json()) || {};
+    const contentType = c.req.header('ContentType');
+    console.log('contentType', contentType);
+    return c.json({
+      message: 'Hello Hono!',
+      contentType: contentType,
+    });
+  } catch (error) {
+    return c.json({ error: 'Invalid or empty JSON in request body' });
+  }
+});
+app.post('/', async (c) => {
+  try {
+    let body = (await c.req.json()) || {};
+    // const contentType = c.req.header('ContentType');
+    console.log('body', body);
+    return c.json({
+      message: 'Hello Hono!',
+      body,
+    });
+  } catch (error) {
+    return c.json({ error: 'Invalid or empty JSON in request body' });
+  }
+});
 
-export default app
+export default app;
